5c9233d06d29e25e2eaf44659eb9aba83030af6a
Stephans-1 <kukustephansneriskwei@gmail.com>
2026-06-05 18:28:23 +0000

    Delete ChurchApp directory

--- ChurchApp/Utility/StaticData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ChurchApp.Utility
-{
-    public static class StaticData
-    {
-        public static string GenderMale { get; set; } = "Male";
-        public static string GenderFemale { get; set; } = "Female";
-    }
-}
--- ChurchApp/wwwroot/js/toastnoty.js
+++
@@ -1,8 +0,0 @@
-﻿window.showToast = function (message, type) {
-    if (type == "success") {
-        toastr.success(message)
-    }
-    if (type == "error") {
-        toastr.error(message)
-    }
-}
